8f9f595349e84281435ea3c35f6a6c176f94af1b
Ian Kersz <kerszamaral@gmail.com>
2022-02-21 19:40:21 -0300

    Aula do dia 24/02

---
+++ Algoritmos e Programação/Semana 6/lab 6/ian_kersz1.c
@@ -0,0 +1,17 @@
+/* Ian Kersz - Cartão ufrgs: 00338368
+24.02.22
+
+in: 2 valores
+out: 1 valor 
+*/
+
+#include<stdio.h>
+#include<math.h>
+#include<ctype.h>
+#include<string.h>
+
+int main(void){
+
+
+    return 0;
+}
