7c9ff0b9d2a19a004a774615fef58641feced021
Ian Kersz <kerszamaral@gmail.com>
2022-02-22 12:17:25 -0300

    Otimizado algoritmo de sort

--- Algoritmos e Programação/Semana 6/aulas/Testes Arrays/Sort.c
+++ Algoritmos e Programação/Semana 6/aulas/Testes Arrays/Sort.c
@@ -18,10 +18,21 @@ int main(void){
     for (i = 0; i < ELEMENTOS; i++){
         scanf(" %c", &Array[i]);
     }
-    
+
+    //Sort para crescente (Acho que acabou ficando quase igual ao bubble sort, mas fiz sem olhar)
+    for(j=0;j<ELEMENTOS*ELEMENTOS; j++){
+        for (i = 0; i<ELEMENTOS-1; i++){
+            if(Array[i]>Array[i+1]){
+                char x = Array[i+1];
+                Array[i+1] = Array[i];
+                Array[i]=x;
+            }
+        } 
+    }
+
     //Output
     printf("\n\nPalavra em ordem alfabetica: ");
-    for(i=0; i<2*ELEMENTOS; i++){
+    for(i=0; i<ELEMENTOS; i++){
         printf("%c ", Array[i]);
     }
 
