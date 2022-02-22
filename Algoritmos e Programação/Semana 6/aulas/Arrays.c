df143a031dbe41b5ae252aa19a8c7dde2efaf0ba
Ian Kersz <kerszamaral@gmail.com>
2022-02-22 02:06:58 -0300

    Adicionado Sort e mais 2 outputs/Better comments

--- Algoritmos e Programação/Semana 6/aulas/Arrays.c
+++ Algoritmos e Programação/Semana 6/aulas/Arrays.c
@@ -1,9 +1,11 @@
 /* Ian Kersz - Cartão ufrgs: 00338368
 21.02.22
-Entra com 10 valores quaisquer, sendo 5 deles par e 5 deles impar, Ordena ele em ordem de entrada
-e da o output em alternadas entre par e impar.
+Entra com 10 valores quaisquer, sendo 5 deles par e 5 deles impar, forma um Array com todos
+Triplica esse Array e então faz um sort crescente e decrescente.
+A output é então o Array original, com a ordem de entrada alternadas entre par e impar, 
+o Array Crescente e o Array Decrescente.
 in: 10 valores
-out: 10 valores
+out: 3 valores (3 arrays de 10 valores, totalizando 30 valores)
 */
 
 #include<stdio.h>
@@ -11,9 +13,11 @@ out: 10 valores
 
 int main(void){
 
-    int i=0, j=0, q=0;   //Contadores
+    int i=0, j=0;   //Contadores
     int Par=0, Impar=0, Valor=0, CounterPar=0, CounterImpar=0;  //Variaveis usadas
-    int APar[ELEMENTOS]={}, AImpar[ELEMENTOS]={}, ArrayFinal[2*ELEMENTOS];  //Arrays
+    //Arrays
+    int APar[ELEMENTOS]={}, AImpar[ELEMENTOS]={};
+    int ArrayFinal[2*ELEMENTOS]={}, ArrayFinalCre[2*ELEMENTOS]={}, ArrayFinalDe[2*ELEMENTOS]={};
 
     //Info do Programa
     printf("\nInforme %d numeros pares e %d numeros impares em qualquer ordem!\n", ELEMENTOS, ELEMENTOS);
@@ -58,24 +62,55 @@ int main(void){
         ArrayFinal[i+1]=AImpar[i/2];
     }
 
-    //Sort (Ainda não está funcionando)
-    /*for (i = 0; i<2*ELEMENTOS-1; i++){
-        if(ArrayFinal[i]>ArrayFinal[i+1]){
-            int x = ArrayFinal[i];
-            int y = ArrayFinal[i+1];
-            ArrayFinal[i] = y;
-            ArrayFinal[i+1]=x;
-        }
-    }*/
+    //Triplicação de ArrayFinal para termos 3 outputs em ordens diferentes
+    for (i = 0; i <2*ELEMENTOS; i++){
+        ArrayFinalCre[i]=ArrayFinal[i];
+        ArrayFinalDe[i]=ArrayFinal[i];
+    }
 
-    //Output: Print do array final
-    printf("\n");
-    printf("Resultado: ");
+    //Sort para crescente (Acho que acabou ficando quase igual ao bubble sort, mas fiz sem olhar)
+    for(j=0;j<ELEMENTOS*ELEMENTOS; j++){
+        for (i = 0; i<2*ELEMENTOS-1; i++){
+            if(ArrayFinalCre[i]>ArrayFinalCre[i+1]){
+                int x = ArrayFinalCre[i];
+                int y = ArrayFinalCre[i+1];
+                ArrayFinalCre[i] = y;
+                ArrayFinalCre[i+1]=x;
+            }
+        } 
+    }
+    //Sort para decrescente
+    for(j=0;j<ELEMENTOS*ELEMENTOS; j++){
+        for (i = 0; i<2*ELEMENTOS-1; i++){
+            if(ArrayFinalDe[i]<ArrayFinalDe[i+1]){ //Alterando o sinal mudamos de crescente pra descrente
+                int x = ArrayFinalDe[i];
+                int y = ArrayFinalDe[i+1];
+                ArrayFinalDe[i] = y;
+                ArrayFinalDe[i+1]=x;
+            }
+        } 
+    }
+
+    //Output
+    //Print do ArrayFinal na ordem de entrada alternada entre par e impar (problema original da aula)
+    printf("\nResultado em ordem inicial: ");
     for(i=0; i<2*ELEMENTOS; i++){
         printf("%d ", ArrayFinal[i]);
     }
-    //Para poder ler quando aberto na cmd
-    printf("\nDigite 'q' para sair: ");
-    scanf("\n%d", &q);
+    //Print do ArrayFinal em ordem crescente (demonstração do sort)
+    printf("\nResultado em ordem crescente: ");
+    for(i=0; i<2*ELEMENTOS; i++){
+        printf("%d ", ArrayFinalCre[i]);
+    }
+    //Print do ArrayFinal em ordem decrescente (demonstração do sort)
+    printf("\nResultado em ordem decrescente: ");
+    for(i=0; i<2*ELEMENTOS; i++){
+        printf("%d ", ArrayFinalDe[i]);
+    }
+
+    //Para poder ler os resultados quando aberto na cmd do windows
+    printf("\n\nDigite qualquer coisa para sair: ");
+    scanf("%d", &j);
+
     return 0;
 }
