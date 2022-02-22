3d7b7584c7bd6075c9c0d525668b8bf5cdcb4ece
Ian Kersz <kerszamaral@gmail.com>
2022-02-22 18:30:59 -0300

    Função criada e funcionando

--- Algoritmos e Programação/Semana 6/aulas/Testes Arrays/Functions.c
+++ Algoritmos e Programação/Semana 6/aulas/Testes Arrays/Functions.c
@@ -1,5 +1,5 @@
 /* Ian Kersz
-20.01.22
+22.02.22
 Pegar o ano de nascimento e subtair o ano do pc para conseguir a idade
 in: ano (1 val)
 out: idade (1 val)
@@ -8,21 +8,32 @@ out: idade (1 val)
 #include <time.h>
 #include <stdlib.h>
 
-int main(){
-    int a, aa, s;
-    //definir ano
-    time_t tempo ;
-    char stempo [100] ;
+int tempoo(void){
+    //variaveis
+    char stempo [100];
+    int result;
+
+    //String manipulation
+    time_t tempo;
     time(&tempo); 
     sprintf(stempo, "%s", 20+ctime(&tempo));
-    aa = atoi(stempo);
+
+    //output
+    result=atoi(stempo);
+    return result;
+}
+
+int main(){
+    int ano=0, anoatual=0, idade=0;
+    //definir ano
+    anoatual = tempoo();
     //input
     printf("\nEm que ano voce nasceu? ");
-    scanf("%d", &a);
+    scanf("%d", &ano);
     //math
-    s = aa - a;
+    idade = anoatual - ano;
     //output
-    printf("\nVoce tem %d anos de idade", s);
+    printf("\nVoce tem %d anos de idade", idade);
 
     return 0;
 }
