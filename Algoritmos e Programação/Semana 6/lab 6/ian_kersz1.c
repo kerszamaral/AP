22a12ce58d4078582dc81d8d5a4749ea27a40f22
Ian Kersz <kerszamaral@gmail.com>
2022-02-24 15:17:43 -0300

    Finalização do lab do dia 24/02

--- Algoritmos e Programação/Semana 6/lab 6/ian_kersz1.c
+++ Algoritmos e Programação/Semana 6/lab 6/ian_kersz1.c
@@ -1,17 +1,42 @@
 /* Ian Kersz - Cartão ufrgs: 00338368
 24.02.22
-
-in: 2 valores
-out: 1 valor 
+Ler uma string de até 25 caracteres, criar duas outras strings, uma toda maiuscula e outra invertida.
+in: 1 string
+out: 2 strings 
 */
 
 #include<stdio.h>
-#include<math.h>
 #include<ctype.h>
 #include<string.h>
 
 int main(void){
+    //variaveis
+    int i=0, j=0, k=0;
+    char palavra[25+1]={0}, reversa[25+1]={0}, maiuscula[25+1]={0};
 
+    //Inicialização
+    printf("Entre com uma string: ");
+
+    //função para pegar a string com espaços
+    if(fgets(palavra, 15, stdin)!=NULL){
+        palavra[strlen(palavra)-1]='\0';
+    }else{
+        printf("Problema com o fgets()\n");
+    }
+
+    //loop para todas maiuscula
+    for (i = 0; i < 26; i++){
+        maiuscula[i]=toupper(palavra[i]);
+    }
+
+    //loop para invertar a paalvra
+    for (j = strlen(palavra)-1; j >= 0; j--){
+        reversa[j]=palavra[k];
+        k++;
+    }
+
+    printf("\nString toda maiuscula: %s", maiuscula);
+    printf("\nString invertida: %s", reversa);
 
     return 0;
 }
--- Algoritmos e Programação/Semana 6/lab 6/ian_kersz2.c
+++ Algoritmos e Programação/Semana 6/lab 6/ian_kersz2.c
@@ -1,17 +1,53 @@
 /* Ian Kersz - Cartão ufrgs: 00338368
 24.02.22
-
-in: 2 valores
-out: 1 valor 
+Pega até 10 valores, faz seus quadrados, faz suas raizes, mostra a media dos quadrados e das raizes,
+informa quantos valores estão contidos entre as medias
+in: até 10 valores
+out: 3 valor (media quadrados, media raizes, Quant num entre as medias) 
 */
 
 #include<stdio.h>
 #include<math.h>
-#include<ctype.h>
 #include<string.h>
 
-int main(void){
+#define MAXLIDOS 10
 
+int main(void){
+    //Variaveis
+    int i=0, j=0, counter=0;
+    float valor=0, QuadradoTotal=0, RaizTotal=0, ArrayQuadrado[MAXLIDOS]={0}, ArrayRaiz[MAXLIDOS]={0};
+
+    //Inicialização
+    printf("Informe até %d valores: ", MAXLIDOS);
+    //Entrada dos valores nos arrays com seus valores respectivos dentro do especificado
+    do{
+        scanf("%f", &valor);
+        if (valor > 0){
+            ArrayQuadrado[i] = pow(valor,2);
+            ArrayRaiz[i] = sqrt(valor);
+            i++;
+        }
+    } while (valor > 0 && i < MAXLIDOS);
+
+    //Calculo da media
+    for (j = 0; j <= i; j++){
+        QuadradoTotal+=ArrayQuadrado[j];
+        RaizTotal+=ArrayRaiz[j];
+    }
+    QuadradoTotal=QuadradoTotal/i;
+    RaizTotal=RaizTotal/i;
+
+    //Calculo valores entre os valores
+    for (j = 0; j < i; j++){
+        if ((ArrayQuadrado[j]>=RaizTotal&&ArrayQuadrado[j]<=QuadradoTotal)||(ArrayRaiz[j]>=RaizTotal&&ArrayRaiz[j]<=QuadradoTotal)){
+            counter++;
+        }
+    }
+
+    //Outputs
+    printf("\nMedia dos quadrados: %f", QuadradoTotal);
+    printf("\nMedia das raizes quadradas: %f", RaizTotal);
+    printf("\nQuantidade de valores entre as duas medias: %d", counter);
 
     return 0;
 }
